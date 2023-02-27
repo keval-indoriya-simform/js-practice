3c48b6bd33d3bfb2dd3621db27c9aed554165875
keval indoriya <keval.i@simformsolutions.com>
2023-02-27 13:26:45 +0530

    javascript file added

--- main.js
+++ main.js
@@ -186,48 +186,131 @@ console.log(person.fullName());
 
 
 // string methods
-let testText = "this is just a random text"
+let testText = "this is just a random text just a random text";
 // String length
 length = testText.length;
-console.log(length)
+console.log(length);
 
 // String slice()
-s1 = testText.slice(3,15)
-console.log(s1)
+s1 = testText.slice(3,15);
+console.log(s1);
 
-s1 = testText.slice(-15,-3)
-console.log(s1)
+s1 = testText.slice(-15,-3);
+console.log(s1);
 
 // String substring()
-s1 = testText.substring(5, 15)
-console.log(s1)
+s1 = testText.substring(5, 15);
+console.log(s1);
 
 // String substr()
 //it takes 7 charecter from 5th index
-s1 = testText.substr(5, 7)
-console.log(s1)
+s1 = testText.substr(5, 7);
+console.log(s1);
 
 // String replace()
 //The replace() method replaces only the first match
 // String replaceAll() replace all match
-s1 = testText.replace("random", "randomly random")
-console.log(s1)
+s1 = testText.replace("random", "randomly random");
+console.log(s1);
 
 // String toUpperCase()
-s1 = testText.toUpperCase()
-console.log(s1)
+s1 = testText.toUpperCase();
+console.log(s1);
 
 // String toLowerCase()
-s1 = testText.toLowerCase()
-console.log(s1)
+s1 = testText.toLowerCase();
+console.log(s1);
 
 // String concat()
+let str1 = "hello";
+let str2 = "there";
+let str3 = str1.concat(" ", str2);
+console.log(str3);
 
 // String trim()
+let trim_str = "        this is trim string         "; 
+s1 = trim_str.trim();
+console.log(s1);
+
 // String trimStart()
+s1 = trim_str.trimStart();
+console.log(s1);
+
 // String trimEnd()
+s1 = trim_str.trimEnd();
+console.log(s1);
+
 // String padStart()
+s1 = str1.padStart(8, "h");
+console.log(s1);
+
 // String padEnd()
+s1 = str1.padEnd(8, "o");
+console.log(s1);
+
 // String charAt()
+s1 = str1.charAt(0);
+console.log(s1);
+
+// s1[0] = "a"
+//gives no error but does not work
+// read only can not modify
+// console.log(s1);
+
 // String charCodeAt()
+s1 = str1.charCodeAt(0);
+console.log(s1);
+
 // String split()
+let str_array = testText.split(" ");
+console.log(str_array);
+
+
+// testText = "this is just a random text just a random text"
+// String indexOf()
+// Both indexOf(), and lastIndexOf() return -1 if the text is not found:
+console.log(testText.indexOf("just"));
+
+// String lastIndexOf()
+console.log(testText.lastIndexOf("just"));
+// console.log(testText.lastIndexOf("just", 35)); 
+
+// String search()
+console.log(testText.search("random"));
+
+// String match()
+console.log(testText.match(/just/g));
+
+// String matchAll()
+const itr = testText.matchAll(/just/g);
+console.log(Array.from(itr));
+
+// String includes()
+// retuen true or false
+console.log(testText.includes("random"));
+
+
+// String startsWith()
+console.log(testText.startsWith("this"));
+// console.log(testText.startsWith("is", 5));
+// start comparing from 5th positions
+
+// String endsWith()
+console.log(testText.endsWith("text"));
+
+
+
+
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue",
+//     fullName: function() {
+//         return this.firstName + " " + this.lastName;
+//     }
+//   };
+
+s1 = `Hello, ${person.fullName()}`;
+console.log(s1);
